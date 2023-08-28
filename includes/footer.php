371cee0758d7405555d8d937b9acbcdb9e33b92c
Sufyann34 <135757783+Sufyann34@users.noreply.github.com>
2023-08-28 00:22:28 -0700

    newigs

---
+++ includes/footer.php
@@ -0,0 +1,46 @@
+<footer>
+   <div class="container">
+      <div class="row text-center">
+         <div class="col-md-4">
+            <div class="widget">
+               <img src="assets/images/insta.png" alt="">
+               <img src="assets/images/fb.png" alt="">
+               <img src="assets/images/li.png" alt="">
+               
+            </div>
+         </div>
+         <div class="col-md-4">
+            <div class="widget">
+               <img src="assets/images/Logo.png" alt="">
+               
+            </div>
+         </div>
+         <div class="col-md-4">
+            <div class="widget">
+               <a href="#" class="btn-a">United states-English</a>
+               
+            </div>
+         </div>
+      </div>
+      <div class="row text-center">
+         <div class="col-md-12">
+            <div class="footcont">
+               <p>© 2021 Innovative Game Studio, Inc.: A LEAGUE OF LEGENDS STORY and any associated logos are trademarks, service marks, and/or registered trademarks of nnovative Game Studio, Inc. Developed by Airship Syndicate. “PlayStation”, “PS5 logo" and “PS4 logo” are registered trademarks or trademarks of Sony Interactive Entertainment Inc. Nintendo Switch is a trademark of Nintendo. Steam and Steam logo are trademarks and/or registered trademarks of Valve Corporation in the U.S. and/or other countries. Microsoft, the Xbox Spheremark, the Series X logo, Series S logo, Series X|S logo, Xbox One, Xbox Series X, Xbox Series S, and Xbox Series X|S are trademark of the Microsoft group of companies.© 2020 Epic Games, Inc. All rights reserved. Epic, Epic Games, Epic Games Store, and their respective logos are trademarks or registered trademarks of Epic Games, Inc. in the U.S. Patent and Trademark Office and elsewhere.</p>
+
+            </div>
+
+
+         </div>
+
+      </div>
+      
+
+      
+      
+   </div>
+</footer>
+<div class="copyright text-center">
+   <div class="container">
+      <p>All Rights Reserved</p>
+   </div>
+</div>
---
+++ includes/header.php
@@ -0,0 +1,30 @@
+<header>
+	<div class="main-header">
+		<div class="container">
+			<div class="menu-Bar">
+				<span></span>
+				<span></span>
+				<span></span>
+			</div>
+			<div class="row align-items-center">
+				<div class="col-md-4 text-left">
+					<a href="./" class="logo">
+						<img src="assets/images/logo.png" alt="">
+					</a>
+				</div>
+			
+				<div class="col-md-8 text-right">
+					<div class="menuWrap">
+						<ul class="menu">
+						    <li class="active"><a href="./">HOME</a></li>
+							<li><a href="#">ABOUT US</a></li>
+							<li><a href="#">SERVICES</a></li>
+							<li><a href="#">OUR WORK</a></li>
+							<li><a href="#">CONTACT US</a></li>
+						</ul>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</header>
